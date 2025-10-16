876a9e5452b424b1222fc49306a404d84f24288d
Criativista <criativistamkg@gmail.com>
2025-10-16 17:11:23 +0000

    https://i.ibb.co/fV5L2chB/logo-thin-last-1-1.png mete este

--- src/components/icons/HairlinesLogo.tsx
+++ src/components/icons/HairlinesLogo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const HairlinesLogo = ({ className }: { className?: string }) => (
     <div className={cn("relative w-40 h-10", className)}>
         <Image 
-            src="https://i.ibb.co/rKSNmWCT/logo-thin-last-1-1.png" 
+            src="https://i.ibb.co/fV5L2chB/logo-thin-last-1-1.png" 
             alt="Hairline Studio Logo" 
             fill
             className="object-contain"
