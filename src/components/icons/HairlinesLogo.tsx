3123efeede9dcebe47a81935965054f426f0e572
Criativista <criativistamkg@gmail.com>
2025-10-16 17:10:42 +0000

    ainda esta igual

--- src/components/icons/HairlinesLogo.tsx
+++ src/components/icons/HairlinesLogo.tsx
@@ -2,12 +2,11 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 export const HairlinesLogo = ({ className }: { className?: string }) => (
-    <div className={cn("flex items-center gap-2", className)}>
+    <div className={cn("relative w-40 h-10", className)}>
         <Image 
             src="https://i.ibb.co/rKSNmWCT/logo-thin-last-1-1.png" 
             alt="Hairline Studio Logo" 
-            width={160} 
-            height={40} 
+            fill
             className="object-contain"
         />
     </div>
