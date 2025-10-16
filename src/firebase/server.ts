16757e8c7a21bcacb58c0e8bc67e5f384fe6171b
Criativista <criativistamkg@gmail.com>
2025-10-16 17:36:25 +0000

    Try fixing this error: `Runtime Error: Error: A "use server" file can on

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,5 +1,3 @@
-'use server';
-
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase-admin/app';
 import { getAuth, type Auth } from 'firebase-admin/auth';
 import { getFirestore, type Firestore } from 'firebase-admin/firestore';
