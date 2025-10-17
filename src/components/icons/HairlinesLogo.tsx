41bb6b5524e79a56809011ae6425667d65e38a22
Criativista <criativistamkg@gmail.com>
2025-10-17 14:24:05 +0000

    Image with src "https://i.ibb.co/fV5L2chB/logo-thin-last-1-1.png" has "f

--- src/components/icons/HairlinesLogo.tsx
+++ src/components/icons/HairlinesLogo.tsx
@@ -7,6 +7,7 @@ export const HairlinesLogo = ({ className }: { className?: string }) => (
             src="https://i.ibb.co/fV5L2chB/logo-thin-last-1-1.png" 
             alt="Hairline Studio Logo" 
             fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             className="object-contain"
         />
     </div>
